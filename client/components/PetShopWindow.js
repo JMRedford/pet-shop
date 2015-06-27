c4c1cefe83f7572400f25a77ee3dbe3dc44ad851
MKS-students <mks.students@hackreactor.com>
2015-06-26 19:21:08 -0500

    initial progress

--- client/components/PetShopWindow.js
+++ client/components/PetShopWindow.js
@@ -8,10 +8,61 @@ PetShopWindow.controller = function () {
   var ctrl = this
   ctrl.shop = m.prop(null)
   Shop.fetch().then(ctrl.shop)
+  ctrl.pets = m.prop(null)
+  Shop.fetchPets().then(ctrl.pets)
+
+  ctrl.apiKey = m.prop(null);
+
+
+  // ctrl.login = function(username, password){
+  ctrl.signIn = function(username,password) {
+    Shop.signIn(username,password);
+  }
+  //   Shop.login(username, password);
+  // }
+
+  // ctrl.signup = function(username, password){
+  //   Shop.signup(username, password);
+  // }
 }
 
 PetShopWindow.view = function (ctrl) {
-  return m('.pet-shop', [
-    m('h1', "Welcome to " + ctrl.shop().name)
-  ])
+  // var likeButton = m();  // if logged in create like button, if not, create blank div
+  // if (ctrl.apiKey()){
+  //   likeButton = m('button')
+  // }
+
+  var elementArray = ctrl.pets().map(function(pet,i) {
+    return m('.pet',{
+      "margin-bottom": "100px"
+
+    },"Name: "+ pet.name,[
+      m('.petSpecies', "Species: "+ pet.species),
+      m('.likes', "Likes: "+ pet.likes.length),
+      //likeButton,
+      m('img', {
+        src: pet.imageUrl,
+        height: "500px",
+        width : "500px"
+      })
+    ])
+  })
+
+  if (!ctrl.apiKey()) {
+
+    elementArray.unshift(m('button', 'Sign In', { onclick: function() {
+      ctrl.signIn();
+    }}))
+    elementArray.unshift(m('input', { type: "text",  id: "passText"}))
+    elementArray.unshift(m('input', { type: "text", id: "userText"}))
+  }
+
+  elementArray.unshift(m('h1', "Welcome to " + ctrl.shop().name));
+  //if (!ctrl.apiKey()){
+  //   elementArray.unshift(m('button','Sign Up'));
+  //   elementArray.unshift(m('button','Sign In'))
+  // }
+
+  return m('.pet-shop', elementArray);
 }
+
--- client/models/shop.js
+++ client/models/shop.js
@@ -4,5 +4,23 @@ var m = require('mithril')
 var Shop = module.exports = {}
 
 Shop.fetch = function () {
-  return m.request({ method: 'GET', url: 'http://pet-shop.api.mks.io/shops/1' })
+  return m.request({ method: 'GET', url: 'http://pet-shop.api.mks.io/shops/1' });
+}
+
+Shop.fetchPets = function () {
+  return m.request({ method: 'GET', url: 'http://pet-shop.api.mks.io/shops/1/pets' });
+}
+
+// Shop.signUp = function(username, password) {
+//   return m.request({ method: 'POST', url: })
+// }
+
+// Shop.logIn = function(username, password){
+//   return m.request({ method: 'POST'})
+// }
+Shop.signIn = function(username,password) {
+  return m.request({method: 'POST', url: 'http://pet-shop.api.mks.io/signin', data: {
+    username: username,
+    password: password
+  }});
 }
